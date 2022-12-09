05fade6b9cfa51a22169eb78add270de4d11f43f
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-09 18:58:33 +0530

    Adding js program.

---
+++ js/age.js
@@ -0,0 +1,15 @@
+var a=parseInt(prompt("enter a age"));
+var b=parseInt(prompt("enter b age"));
+var c=parseInt(prompt("enter c age"));
+if (b<a && c<a)
+{
+alert("a is older")
+}
+else if(a<b && c<b)
+{
+alert("b is older")
+}
+else
+{
+alert("c is older")-
+}	
